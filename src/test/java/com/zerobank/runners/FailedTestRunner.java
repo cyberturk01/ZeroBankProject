35a8c807992a1f33bd6693688fb468db70cab49a
Gokhan Yigit <yigitgokhan1@gmail.com>
2020-02-27 10:28:26 +0100

    New features

--- src/test/java/com/zerobank/runners/FailedTestRunner.java
+++ src/test/java/com/zerobank/runners/FailedTestRunner.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-//        features = "@target/rerun.txt",
-////        glue="com/zerobank/step_definitions"
+        features = "@target/rerun.txt",
+        glue = "com/zerobank/step_definitions"
 
 )
 public class FailedTestRunner {
